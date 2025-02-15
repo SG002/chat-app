82577a14fd1412f64adaa78bb192323685c4518d
Sanjosh Gahlyan <sanjoshgahlyan02@gmail.com>
2025-02-15 16:10:25 +0530

    update

--- chat-app-backend/config/plugins.ts
+++ chat-app-backend/config/plugins.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   'users-permissions': {
     config: {
       jwt: {
-        secret: env('PLUGIN_USERS_PERMISSIONS_JWT_SECRET')
+        secret: env('JWT_SECRET')
       }
     }
   }
